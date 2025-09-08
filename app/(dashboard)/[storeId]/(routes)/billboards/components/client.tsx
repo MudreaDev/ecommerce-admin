c92b519261c5e9ab154822475bbf0e6cea95e385
Mihai <mihail.mudrea@isa.utm.md>
2025-09-08 15:11:01 +0300

    upload !work

--- app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
@@ -4,8 +4,11 @@ import { Button } from "@/components/ui/button";
 import { Heading } from "@/components/ui/heading";
 import { Separator } from "@/components/ui/separator";
 import { Plus } from "lucide-react";
+import { useParams, useRouter } from "next/navigation";
 
 export const BillboardsClient = () => {
+    const router = useRouter();
+    const params = useParams();
     return (
         <>
         <div className="flex items-center justify-between">
@@ -13,8 +16,8 @@ export const BillboardsClient = () => {
                 title="Billboards (0)"
                 description="Manage billboards for your store"
             />
-            <Button>
-                <Plus className="mr2 h-4 w-4" />
+            <Button onClick={() => router.push(`/${params.storeId}/billboards/new`)}>
+                <Plus className="mr-2 h-4 w-4" />
                 Add new
             </Button>
         </div>

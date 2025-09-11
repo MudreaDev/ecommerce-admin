de08b3348b718160757969bea859385361d0c0bb
Mihai <mihail.mudrea@isa.utm.md>
2025-09-11 22:22:44 +0300

    commit

--- app/api/[storeId]/categories/[categoryId]/route.ts
+++ app/api/[storeId]/categories/[categoryId]/route.ts
@@ -22,6 +22,9 @@ export async function GET(
                 id: params.categoryId,
               
             },
+            include: {
+                billboard: true,
+            },
         });
 
         return NextResponse.json(category);
